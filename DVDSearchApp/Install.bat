0cb0e09ca757dd464cdc0af2938664b080bb0de2
Vic Gullotti <viktoriagullotti@hotmail.com>
2017-06-21 13:41:22 +0800

    Original project

---
+++ DVDSearchApp/Install.bat
@@ -0,0 +1,6 @@
+REM ### Make sure you create a manifest file (e.g. manifest.mf) before running this
+
+jar cfm DVDSearchApp.jar manifest.mf build\classes\dvd\search\app\*.class
+
+:exit
+pause
---
+++ DVDSearchApp/Uninstall.bat
@@ -0,0 +1,1 @@
+del DVDSearchApp.jar
